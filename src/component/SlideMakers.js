d9a4ac1b43a86707224c3564162d0f74a9907933
Aamir Aziz <aamir.aziz220@gmail.com>
2023-03-29 13:28:17 +0530

    added slider value

---
+++ src/component/SlideMakers.js
@@ -0,0 +1,169 @@
+const SlideMakers = {
+
+    Loan: [
+        {
+            value: 1000000,
+            label: '10 L',
+        },
+        {
+            value: 2000000,
+            label: '20 L',
+        },
+        {
+            value: 3000000,
+            label: '30 L',
+        },
+        {
+            value: 4000000,
+            label: '40 L',
+        },
+        {
+            value: 5000000,
+            label: '50 L',
+        },
+        {
+            value: 6000000,
+            label: '60 L',
+        },
+        {
+            value: 7000000,
+            label: '70 L',
+        },
+        {
+            value: 8000000,
+            label: '80 L',
+        },
+        {
+            value: 9000000,
+            label: '90 L',
+        },
+        {
+            value: 10000000,
+            label: '1 Cr',
+        },
+
+    ],
+
+
+    intrest: [
+        {
+            value: 0,
+            label: '0%',
+        },
+
+        {
+            value: 5,
+            label: '5%',
+        },
+
+        {
+            value: 10,
+            label: '10%',
+        },
+
+        {
+            value: 15,
+            label: '15%',
+        },
+
+        {
+            value: 20,
+            label: '20%',
+        },
+
+        {
+            value: 25,
+            label: '25%',
+        },
+        {
+            value: 30,
+            label: '30%',
+        },
+        {
+            value: 35,
+            label: '35%',
+        },
+        {
+            value: 40,
+            label: '40%',
+        },
+        {
+            value: 45,
+            label: '45%',
+        },
+        {
+            value: 50,
+            label: '50%',
+        },
+
+    ],
+
+
+
+    Tenure: [
+
+        {
+            value: 12,
+            label: "12 M",
+        },
+
+        {
+            value: 24,
+            label: "24 M",
+        },
+
+        {
+            value: 36,
+            label: "36 M",
+        },
+
+        {
+            value: 48,
+            label: "48 M",
+        },
+
+        {
+            value: 60,
+            label: "60 M",
+        },
+
+        {
+            value: 72,
+            label: "72 M",
+        },
+
+
+        {
+            value: 84,
+            label: "84 M",
+        },
+
+        {
+            value: 96,
+            label: "96 M",
+        },
+
+        {
+            value: 108,
+            label: "108 M",
+        },
+
+        {
+            value: 120,
+            label: "120 M",
+        },
+
+        {
+            value: 340,
+            label: "340",
+        },
+
+        {
+            value: 365,
+            label: "365",
+        },
+    ],
+
+}
+
+export default SlideMakers;
--- src/component/emiCalculator.jsx
+++ src/component/emiCalculator.jsx
@@ -1,18 +1,19 @@
 import React, { useState, useRef, useEffect } from 'react'
 import classes from './emiCalculator.module.scss'
-import { Slider, Typography, Tooltip } from '@mui/material';
+import { Slider, Typography } from '@mui/material';
 import { Table, TableCell, TableRow, TableHead } from '@material-ui/core';
 import Chart from 'chart.js/auto';
 import { v4 as uuidv4 } from 'uuid';
+import SlideMakers from './SlideMakers'
 
 
 
 function EmiCalculator() {
 
 
-  const [loanAmount, setloanAmount] = useState(50000);
-  const [intrestRate, setIntrestRate] = useState(6)
-  const [timePeriod, setTimePeriod] = useState(6)
+  const [loanAmount, setloanAmount] = useState(1000000);
+  const [intrestRate, setIntrestRate] = useState(5)
+  const [timePeriod, setTimePeriod] = useState(12)
   const [emi, setEmi] = useState()
   const [totalInterest, setTotalInterest] = useState(0)
   const [totalAmount, setTotalAmount] = useState(0)
@@ -20,7 +21,6 @@ function EmiCalculator() {
 
   const canvasRef = useRef(null);
   const chartRef = useRef(null);
-  const [chartData, setChartData] = useState();
 
   useEffect(() => {
     if (chartRef.current) {
@@ -41,6 +41,9 @@ function EmiCalculator() {
 
   const canvasId = uuidv4();
 
+  useEffect(() => {
+    calculateEMI(loanAmount, intrestRate, timePeriod)
+  }, [])
 
 
   const handleChange = (e) => {
@@ -85,18 +88,17 @@ function EmiCalculator() {
 
         <div className={classes.loanAmountSection}>
 
-          <Typography className={classes.loanHeading} >Loan Amount (<b>Rs</b>):</Typography>
+          <Typography className={classes.loanHeading} >Loan Amount (<b>&#8360;</b>):</Typography>
           <Slider
             className={classes.slider}
             sx={{ color: 'success.main', }}
             valueLabelDisplay="auto"
             value={loanAmount}
             onChange={handleChange}
-
             aria-label="custom thumb label"
-            min={50000}
+            min={1000000}
             max={10000000}
-
+            marks={SlideMakers.Loan}
           />
         </div>
 
@@ -109,8 +111,9 @@ function EmiCalculator() {
             value={intrestRate}
             onChange={handleInterestChange}
             aria-label="custom thumb label"
-            min={6}
+            min={5}
             max={50}
+            marks={SlideMakers.intrest}
           />
         </div>
 
@@ -123,8 +126,10 @@ function EmiCalculator() {
             value={timePeriod}
             onChange={handleTimePeriodChange}
             aria-label="custom thumb label"
-            min={6}
+            min={12}
             max={120}
+            marks={SlideMakers.Tenure}
+            Tenure
           />
         </div>
 
@@ -132,50 +137,50 @@ function EmiCalculator() {
       </div>
 
 
+      <div className={classes.tableAndPieChartContainer}>
+        <div className={classes.tableSection}>
+          <Table className={classes.table}>
+            <TableHead >
+              <TableRow>
+                <TableCell className={classes.tableCellText}><b>Loan Amount:</b></TableCell>
+                <TableCell className={classes.tableCellValue}><b><b>&#8360;</b>  {loanAmount}</b></TableCell>
+              </TableRow>
 
-      <div className={classes.tableSection}>
-        <Table className={classes.table}>
-          <TableHead >
-            <TableRow>
-              <TableCell className={classes.tableCellText}><b>Loan Amount:</b></TableCell>
-              <TableCell className={classes.tableCellValue}><b>Rs {loanAmount}</b></TableCell>
-            </TableRow>
+              <TableRow>
+                <TableCell className={classes.tableCellText}><b>Intrest Rate :</b></TableCell>
+                <TableCell className={classes.tableCellValue}><b>{intrestRate}%</b></TableCell>
+              </TableRow>
 
-            <TableRow>
-              <TableCell className={classes.tableCellText}><b>Intrest Rate :</b></TableCell>
-              <TableCell className={classes.tableCellValue}><b>{intrestRate}%</b></TableCell>
-            </TableRow>
+              <TableRow>
+                <TableCell className={classes.tableCellText}><b>Teneure (Month) :</b></TableCell>
+                <TableCell className={classes.tableCellValue}><b>{timePeriod} </b></TableCell>
+              </TableRow>
 
-            <TableRow>
-              <TableCell className={classes.tableCellText}><b>Teneure (Month) :</b></TableCell>
-              <TableCell className={classes.tableCellValue}><b>{timePeriod}</b></TableCell>
-            </TableRow>
+              <TableRow>
+                <TableCell className={classes.tableCellText}><b>Emi :</b></TableCell>
+                <TableCell className={classes.tableCellValue}> <b>&#8360;</b> <b>{emi}</b></TableCell>
+              </TableRow>
 
-            <TableRow>
-              <TableCell className={classes.tableCellText}><b>Emi :</b></TableCell>
-              <TableCell className={classes.tableCellValue} value={emi}><b>{emi}</b></TableCell>
-            </TableRow>
+              <TableRow>
+                <TableCell className={classes.tableCellText}><b>Total Intrest :</b></TableCell>
+                <TableCell className={classes.tableCellValue}><b><b>&#8360;</b>  {totalInterest}</b></TableCell>
+              </TableRow>
 
-            <TableRow>
-              <TableCell className={classes.tableCellText}><b>Total Intrest :</b></TableCell>
-              <TableCell className={classes.tableCellValue}><b>Rs {totalInterest}</b></TableCell>
-            </TableRow>
-
-            <TableRow>
-              <TableCell className={classes.tableCellText}><b>Total Payment: <br></br>  Loan Amount + intrest :</b></TableCell>
-              <TableCell className={classes.tableCellValue}><b>Rs {totalAmount} </b></TableCell>
-            </TableRow>
-          </TableHead>
+              <TableRow>
+                <TableCell className={classes.tableCellText}><b>Total Payment: <br></br>  Loan Amount + intrest :</b></TableCell>
+                <TableCell className={classes.tableCellValue}><b><b>&#8360;</b>  {totalAmount} </b></TableCell>
+              </TableRow>
+            </TableHead>
 
 
-        </Table>
+          </Table>
 
+        </div>
+
+        <div className={classes.pieChart}>
+          <canvas className={classes.pie} id={canvasId} ref={canvasRef} />
+        </div>
       </div>
-
-      <div className={classes.pieChart}>
-        <canvas className={classes.pie} height={'300px'} width={'300px'} id={canvasId} ref={canvasRef} />
-      </div>
-
 
     </div >
   )
